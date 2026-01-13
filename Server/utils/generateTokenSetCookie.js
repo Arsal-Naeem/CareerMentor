939e71f7832e47353ad48a44f51454c516549b58
AbdulWasaySaleem <153143400+AbdulWasaySaleem@users.noreply.github.com>
2026-01-13 06:30:43 +0500

    updated token

--- Server/utils/generateTokenSetCookie.js
+++ Server/utils/generateTokenSetCookie.js
@@ -1,4 +1,7 @@
 import jwt from "jsonwebtoken";
+import {dotenv} from "dotenv"
+
+dotenv.config();
 
 const generateTokenSetCookie = (res, userId,role) => {
   // Generate JWT token
@@ -10,6 +13,7 @@ const generateTokenSetCookie = (res, userId,role) => {
   res.cookie("token", token, {
     httpOnly: true, //XSS atack protection
     secure: process.env.NODE_ENV === "production", // Set to true if using HTTPS
+    sameSite: "none", // Adjust as needed (Lax, Strict, None)
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
