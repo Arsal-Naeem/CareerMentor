f2b19abe118e119adbd40a7211604fcb83d17bed
AbdulWasaySaleem <153143400+AbdulWasaySaleem@users.noreply.github.com>
2026-01-13 06:18:30 +0500

    .

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -2,7 +2,7 @@ import { DataTypes } from "sequelize";
 import { sequelize } from "../config/connectDB.js";
 
 const User = sequelize.define(
-  "User",
+  "user",
   {
     firstName: {
       type: DataTypes.STRING,
